9de1eb43ef8133cc213d4d88a1e86740ca1e0f5d
Dan <khazifire@gmail.com>
2021-05-19 23:31:04 +0700

    updated parking spots

--- main.py
+++ main.py
@@ -17,25 +17,25 @@ print('Connection successfully')
 print(station.ifconfig())
 
 
-def web_page(distance):
-    html = """
+def web_page(distance1,distance2,distance3,counter):
+   html = """
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="refresh" content="5">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Simple Parking System</title>
     </head>
     <body>
         <div id="container">
             <h1>Smart Parking System</h1>
-            <p><strong>Available Parking Spot:</strong>  1 <br><strong>Total Parking Spot:</strong> 3</p>
+            <p><strong>Available Parking Spot:</strong>"""+str(counter)+"""<br><strong>Total Parking Spot:</strong> 3</p>
             <div class="inner" id="inb">
-                <div id="parking1">1</div>
-                <div id="parking2">2 </div>
-                <div id="parking3">3 </div>
-                <p>""" + str(distance) + """ </p>
+                <div id="parking1"><img id="car1" src="https://github.com/dankazim/ParkingSystem/blob/main/car.png?raw=true" alt=""></div>
+                <div id="parking2"><img id="car2" src="https://github.com/dankazim/ParkingSystem/blob/main/car.png?raw=true" alt=""></div>
+                <div id="parking3"><img id="car3" src="https://github.com/dankazim/ParkingSystem/blob/main/car.png?raw=true" alt=""></div> 
             </div>
         </div>
         <style>
@@ -48,50 +48,75 @@ def web_page(distance):
             .inner{padding: 4%;display: grid;grid-template-columns: repeat(auto-fit,minmax(50px, 1fr));grid-gap: 1rem;} 
             .inner>div{height: 7em; border-radius: 25px;display: flex;align-items: center;justify-content: center;}
             #parking1{background-color:var(--green);}
-            #parking2{background-color: var(--red);}
-            #parking3{background-color: var(--red);}
+            #parking2{background-color: var(--green);}
+            #parking3{background-color: var(--green);}
+            img{ height:90%;}
         </style>
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"> </script>
         
         <script>
-       
-            $(document).ready(function(){
-                function refresh(){
-                   distance = """ + str(distance) + """
-            if ( distance  <=29){document.getElementById("parking1").style.backgroundColor='rgb(255, 127, 127)';
-            }else{document.getElementById("parking1").style.backgroundColor='rgb(171, 231, 141)';}
-            console.log(distance)
-                }
-                setInterval(function() {
-                    refresh()
-                }, 2000);
-            }) 
-
-
+            distance1 = """ + str(distance1) + """
+            distance2 = """ + str(distance2) + """
+            distance3 = """ + str(distance3) + """
             
+
+            if ( distance1  <=29){
+            document.getElementById("parking1").style.backgroundColor='rgb(255, 127, 127)';
+            document.getElementById("car1").style.visibility = "visible";
+            }else{document.getElementById("parking1").style.backgroundColor='rgb(171, 231, 141)';
+            document.getElementById("car1").style.visibility = "hidden";}
+            
+            if ( distance2  <=29){
+            document.getElementById("parking2").style.backgroundColor='rgb(255, 127, 127)';
+            document.getElementById("car2").style.visibility = "visible";
+            }else{document.getElementById("parking2").style.backgroundColor='rgb(171, 231, 141)';
+            document.getElementById("car2").style.visibility = "hidden";}
+
+            if ( distance2  <=29){
+            document.getElementById("parking3").style.backgroundColor='rgb(255, 127, 127)';
+            document.getElementById("car3").style.visibility = "visible";
+            }else{document.getElementById("parking3").style.backgroundColor='rgb(171, 231, 141)';
+            document.getElementById("car3").style.visibility = "hidden";}
         </script>
     </body>
     </html>"""
-    return html
+   return html
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
 
-distance = randint(20,60)
+
+counter = 0
 while True:
     conn, addr = s.accept()
     print(' Got a connection from {}'.format(str(addr)))
     request = conn.recv(1024)
     request = str(request)
-    print('Content = {}'.format(request))
+    
+    distance1 = randint(25,35)
+    distance2 = randint(28,40)
+    distance3 = randint(23,35)
+    print(distance1,distance3,distance3)
+#
 
-    print(distance)
-    d = randint(-10,1)
-    distance = distance + d
+    if (distance1 >29 and distance2 >29) and distance3 >29:
+        counter =3
+    elif (distance1 >29 and distance3 >29):
+        couner =2
+    elif (distance2 >29  and distance3 >29):  
+        counter =2
+    elif (distance1 >29 and distance2 >29):
+        counter =2
+    elif distance1 >29  or distance2 >29  or distance3 >29 :
+        counter =1
+    else:
+        counter = 0
+    
     sleep(0.2)
     
+    
 
-    response = web_page(str(distance))
+    response = web_page(str(distance1),str(distance2),str(distance3),str(counter))
     
     conn.send('HTTP/1.1 200 OK\n')
     conn.send('Content-Type: text/html\n')
@@ -99,4 +124,5 @@ while True:
     conn.sendall(response)
     conn.close()
         
+
 
--- t.py
+++ t.py
@@ -5,8 +5,6 @@ import dht
 from random import randint
 from time import sleep
 
-
-
 ssid = 'Kazimoto'
 password = '$Yealink$'
 station = network.WLAN(network.STA_IF)
@@ -19,54 +17,56 @@ print('Connection successfully')
 print(station.ifconfig())
 
 
-def web_page(distance):
+def web_page(distance1):
     html = """
     <!DOCTYPE html>
     <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Simple Parking System</title>
-        </head>
-        <body>
-            <div id="container">
-                <h1>Smart Parking System</h1>
-                <p><strong>Available Parking Spot:</strong>  1 <br><strong>Total Parking Spot:</strong> 3</p>
-                <div class="inner">
-                    <div id="parking1">1</div>
-                    <div id="parking2">2 </div>
-                    <div id="parking3">3 </div>
-                    <p>""" + str(distance) + """ </p>
-                </div>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="refresh" content="5">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Simple Parking System</title>
+    </head>
+    <body>
+        <div id="container">
+            <h1>Smart Parking System</h1>
+            <p><strong>Available Parking Spot:</strong>  1 <br><strong>Total Parking Spot:</strong> 3</p>
+            <div class="inner" id="inb">
+                <div id="parking1"><img id="car" src="https://github.com/dankazim/ParkingSystem/blob/main/car.png?raw=true" alt=""></div>
+                <div id="parking2">2 </div>
+                <div id="parking3">3 </div>
+                <p>""" + str(distance1) + """ </p>
             </div>
-            <style>
-                @import url('https://fonts.googleapis.com/css2?family=Vollkorn:wght@400;500;700&display=swap');
-                
-                body{display: grid; font-family: 'Vollkorn', serif;background-color: #131313;}
-                * {padding: 0;margin: 0;box-sizing: border-box;text-decoration: none;}
-                h1{padding-top: 5%;}
-                #container{margin-top: 10%;border: 2px solid black;border-radius: 30px;place-self: center;padding: 0 60px;height: 50%;background-color: beige;}
-                .inner{padding: 4%;display: grid;grid-template-columns: repeat(auto-fit,minmax(50px, 1fr));grid-gap: 1rem;} 
-                .inner>div{height: 7em; border-radius: 25px;display: flex;align-items: center;justify-content: center;}
-                #parking1{background-color: blue}
-                #parking2{background-color: rgb(255, 127, 127);}
-                #parking3{background-color: rgb(255, 127, 127);}
-            </style>
-             <script>
-             $(document).ready(
-             function(){
-             setInterval(function() {
-             $('#inner');
-             }, 2000); });
-                distance = """ + str(distance) + """
-                if ( distance  <=29){document.getElementById("parking1").style.backgroundColor='rgb(255, 127, 127)';
-                }else{document.getElementById("parking1").style.backgroundColor='rgb(171, 231, 141)';}
-                console.log(distance)
-                
-            </script>
-        </body>
+        </div>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Vollkorn:wght@400;500;700&display=swap');
+            :root{--green:rgb(171, 231, 141); --red:rgb(255, 127, 127);}
+            body{display: grid; font-family: 'Vollkorn', serif;background-color: #131313;}
+            * {padding: 0;margin: 0;box-sizing: border-box;text-decoration: none;}
+            h1{padding-top: 5%;}
+            #container{margin-top: 10%;border: 2px solid black;border-radius: 30px;place-self: center;padding: 0 60px;height: 50%;background-color: beige;}
+            .inner{padding: 4%;display: grid;grid-template-columns: repeat(auto-fit,minmax(50px, 1fr));grid-gap: 1rem;} 
+            .inner>div{height: 7em; border-radius: 25px;display: flex;align-items: center;justify-content: center;}
+            #parking1{background-color:var(--green);}
+            #parking2{background-color: var(--green);}
+            #parking3{background-color: var(--green);}
+            img{ height:90%;}
+        </style>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"> </script>
+        
+        <script>
+            distance = """ + str(distance1) + """
+    console.log(distance)
+            if ( distance  <=29){
+            document.getElementById("parking1").style.backgroundColor='rgb(255, 127, 127)';
+            document.getElementById("car").style.visibility = "visible";
+            
+            }else{document.getElementById("parking1").style.backgroundColor='rgb(171, 231, 141)';
+            document.getElementById("car").style.visibility = "hidden";}
+            
+        </script>
+    </body>
     </html>"""
     return html
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -75,21 +75,19 @@ s.listen(5)
 
 
 
-distance = randint(20,60)
 while True:
     conn, addr = s.accept()
     print(' Got a connection from {}'.format(str(addr)))
     request = conn.recv(1024)
     request = str(request)
-    print('Content = {}'.format(request))
-
-    print(distance)
-    d = randint(-10,1)
-    distance = distance + d
+    
+    distance1 = randint(25,35)
+    print(distance1)
+#   
     sleep(0.2)
     
-   
-    response = web_page(str(distance))
+
+    response = web_page(str(distance1))
     
     conn.send('HTTP/1.1 200 OK\n')
     conn.send('Content-Type: text/html\n')
@@ -97,4 +95,5 @@ while True:
     conn.sendall(response)
     conn.close()
         
+
 
